d7d6532331fada3d05cecc18a5ef190648412adc
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2016-04-17 18:25:32 +0200

    add stuff around bananapi-pro and hdd installtion add checksum fix smaller docu bug

---
+++ bananapi-pro/u-boot/sata_boot/boot.cmd
@@ -0,0 +1,4 @@
+fatload mmc 0 0x46000000 uImage
+fatload mmc 0 0x49000000 sun7i-a20-bananapro.dtb
+setenv bootargs console=ttyS0,115200 console=tty0 loglevel=9 <earlyprintk> root=/dev/sda1 rootwait panic=10 
+bootm 0x46000000 - 0x49000000
