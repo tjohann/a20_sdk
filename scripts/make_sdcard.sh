86767169b214e8936b22ef97565bdcdc7a011a88
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2016-07-19 18:26:16 +0200

    mv documentation links to my_common_doc.git

--- scripts/make_sdcard.sh
+++ scripts/make_sdcard.sh
@@ -598,17 +598,6 @@ menu()
     esac
 }
 
-#
-# Temp store for links realted to bash scripting
-# -> will be moved to my_common_docs.git
-#
-# http://subsignal.org/doc/AliensBashTutorial.html
-# http://www.cc-c.de/german/linux/linux-dialog.php
-# http://www.linuxjournal.com/article/2807?page=0,2
-# http://www.linuxintro.org/wiki/BaBE_-_Bash_By_Examples
-# http://mywiki.wooledge.org/BashFAQ
-# http://superuser.com/questions/829921/how-to-get-status-code-of-program-piped-to-linux-dialog-command
-# http://unix.stackexchange.com/questions/81181/graphically-ask-for-password-in-a-bash-script-and-retain-default-sudo-timeout-se
 
 # ******************************************************************************
 # ***                         Main Loop                                      ***
