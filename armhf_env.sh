cc053a1a4b96efa70b9f104a304ae5aaeb7b3ecf
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2016-07-30 10:55:57 +0200

    update kernel version to 4.6.5 and 4.6.5-rt9

--- armhf_env.sh
+++ armhf_env.sh
@@ -6,9 +6,9 @@
 export MY_HOST_ARCH=$(uname -m)
 
 # set supported kernel version
-export ARMHF_KERNEL_VER=4.6.4
-export ARMHF_RT_KERNEL_VER=4.6.4
-export ARMHF_RT_VER=rt8
+export ARMHF_KERNEL_VER=4.6.5
+export ARMHF_RT_KERNEL_VER=4.6.5
+export ARMHF_RT_VER=rt9
 
 # home of the git repo
 export ARMHF_HOME=/var/lib/a20_sdk
