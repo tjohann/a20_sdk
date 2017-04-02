e7c6cb3163806033d58eeaa558c170f7e62a556f
Thorsten Johannvorderbrueggen <myhalfdan@googlemail.com>
2017-04-02 10:26:27 +0200

    update kernel to 4.9.20 and 4.10.8 for nanopi

--- configs/armhf_env.sh
+++ configs/armhf_env.sh
@@ -6,9 +6,9 @@
 export MY_HOST_ARCH=$(uname -m)
 
 # set supported kernel version
-export ARMHF_KERNEL_VER=4.9.16
-export ARMHF_RT_KERNEL_VER=4.9.13
-export ARMHF_RT_VER=rt12
+export ARMHF_KERNEL_VER=4.9.20
+export ARMHF_RT_KERNEL_VER=4.9.18
+export ARMHF_RT_VER=rt14
 
 # home of the git repo
 export ARMHF_HOME=/var/lib/a20_sdk
