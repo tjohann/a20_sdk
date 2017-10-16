eaf75fe0b87b3b77edc3da9315143ca2fea0776f
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2017-10-16 09:17:55 +0200

    update kernel to 4.14-rc5 for cubietruck-plus update kernel to 4.13.7 update kernel to 4.11.12-rt15 (olimex)

--- configs/armhf_env.sh
+++ configs/armhf_env.sh
@@ -6,9 +6,9 @@
 export MY_HOST_ARCH=$(uname -m)
 
 # set supported kernel version
-export ARMHF_KERNEL_VER=4.13.5
+export ARMHF_KERNEL_VER=4.13.7
 export ARMHF_RT_KERNEL_VER=4.11.12
-export ARMHF_RT_VER=rt14
+export ARMHF_RT_VER=rt15
 
 # home of the git repo
 export ARMHF_HOME=/var/lib/a20_sdk
