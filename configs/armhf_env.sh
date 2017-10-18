c98761693c26eb3227d44ede4a1f358c21f14186
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2017-10-18 13:41:45 +0200

    update kernel to 4.13.7-rt1 (olimex)

--- configs/armhf_env.sh
+++ configs/armhf_env.sh
@@ -7,8 +7,8 @@ export MY_HOST_ARCH=$(uname -m)
 
 # set supported kernel version
 export ARMHF_KERNEL_VER=4.13.7
-export ARMHF_RT_KERNEL_VER=4.11.12
-export ARMHF_RT_VER=rt15
+export ARMHF_RT_KERNEL_VER=4.13.7
+export ARMHF_RT_VER=rt1
 
 # home of the git repo
 export ARMHF_HOME=/var/lib/a20_sdk
