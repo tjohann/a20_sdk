c27d4cbbed32730014af2576a8283c5152bfd067
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2017-11-18 13:12:40 +0100

    update env script

--- configs/armhf_env.sh
+++ configs/armhf_env.sh
@@ -6,9 +6,9 @@
 export MY_HOST_ARCH=$(uname -m)
 
 # set supported kernel version
-export ARMHF_KERNEL_VER=4.13.10
-export ARMHF_RT_KERNEL_VER=4.13.10
-export ARMHF_RT_VER=rt3
+export ARMHF_KERNEL_VER=4.14
+export ARMHF_RT_KERNEL_VER=4.14
+export ARMHF_RT_VER=rt1
 
 # home of the git repo
 export ARMHF_HOME=/var/lib/a20_sdk
