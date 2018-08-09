6eb2ba2adc7e1af23db23c84bd8e1c769a29fe46
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2018-08-09 16:24:09 +0200

    add a lot of forgotten updates ... sry
    update kernel to 4.17.13
    update kernel and devicetree configs
    update kernel to 4.17.12
    update kernel to 4.16.18-rt12 (olimex)

--- configs/armhf_env.sh
+++ configs/armhf_env.sh
@@ -6,9 +6,9 @@
 export MY_HOST_ARCH=$(uname -m)
 
 # set supported kernel version
-export ARMHF_KERNEL_VER=4.17.9
+export ARMHF_KERNEL_VER=4.17.13
 export ARMHF_RT_KERNEL_VER=4.16.18
-export ARMHF_RT_VER=rt10
+export ARMHF_RT_VER=rt12
 
 # home of the git repo
 export ARMHF_HOME=/var/lib/a20_sdk
