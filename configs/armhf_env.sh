112c8e3d43d7132c99c0a546735ece2fec8aefc8
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2020-05-11 19:58:44 +0200

    update kernel to 5.6.12 update kernel to 5.6.10-rt5

--- configs/armhf_env.sh
+++ configs/armhf_env.sh
@@ -6,9 +6,9 @@
 export MY_HOST_ARCH=$(uname -m)
 
 # set supported kernel version
-export ARMHF_KERNEL_VER=5.6.4
-export ARMHF_RT_KERNEL_VER=5.6.2
-export ARMHF_RT_VER=rt1
+export ARMHF_KERNEL_VER=5.6.12
+export ARMHF_RT_KERNEL_VER=5.6.10
+export ARMHF_RT_VER=rt5
 
 # home of the git repo
 export ARMHF_HOME=/var/lib/a20_sdk
