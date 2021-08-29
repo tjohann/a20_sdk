f22e4b5b2e518de440360edf78da553c5a4759fd
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2021-08-29 17:40:27 +0200

    sdk && target stuff:
    - update kernel to 5.10.61 and 5.10.59-rt52
    - udapte kernel configs
    - update toolchain to latest void-linux versions
    - regular updates for glibc and musl image
    - update uboot to v2021.07 (for all devices)
    - prepare A20_SDK_V2.9.5
    - clear/update checksum.sha256

--- configs/armhf_env.sh
+++ configs/armhf_env.sh
@@ -6,9 +6,9 @@
 export MY_HOST_ARCH=$(uname -m)
 
 # set supported kernel version
-export ARMHF_KERNEL_VER=5.10.31
-export ARMHF_RT_KERNEL_VER=5.10.27
-export ARMHF_RT_VER=rt36
+export ARMHF_KERNEL_VER=5.10.61
+export ARMHF_RT_KERNEL_VER=5.10.59
+export ARMHF_RT_VER=rt52
 
 # home of the git repo
 export ARMHF_HOME=/var/lib/a20_sdk
